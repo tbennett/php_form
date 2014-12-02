a9c9ad73db8a19980198272035bd0ed9eb74b4d1
tbennett <tbennett@aii.edu>
2014-12-01 21:30:25 -0500

    A simple email form with php validation

---
+++ inc/footer.inc.php
@@ -0,0 +1,8 @@
+<footer>
+  <h3>Footer</h3>
+  <nav>
+    <a href="#" title="" class="home-lk">home</a>
+    <a href="#" title="" class="about-lk">about</a>
+    <a href="#" title="" class="current contact-lk">contact</a>
+  </nav>
+</footer>
---
+++ inc/header.inc.php
@@ -0,0 +1,8 @@
+<header>
+  <h1>Form Demo</h1>
+  <nav>
+    <a href="#" title="" class="home-lk">home</a>
+    <a href="#" title="" class="about-lk">about</a>
+    <a href="#" title="" class="current contact-lk">contact</a>
+  </nav>
+</header>
